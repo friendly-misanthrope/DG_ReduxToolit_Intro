fd638883fa8ee2c14791ec6ec44f365a390631ad
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-07 22:13:17 -0700

    swapped button positions

--- src/features/counter/CounterView.jsx
+++ src/features/counter/CounterView.jsx
@@ -12,8 +12,8 @@ const CounterView = () => {
     <section>
       <p>{count}</p>
       <div>
-        <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(increment())}>+</button>
       </div>
     </section>
   );
