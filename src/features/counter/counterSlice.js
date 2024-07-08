eb5419ae47d2527647d225190cb726b79d934d66
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-07 23:41:21 -0700

    Added reset and incrementByAmount actions to counterSlice

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -8,14 +8,20 @@ export const counterSlice = createSlice({
   name: 'counter', 
   initialState,
   reducers: {
-    increment: (state, action) => {
+    increment: (state) => {
       state.count += 1;
     },
-    decrement: (state, action) => {
+    decrement: (state) => {
       state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload;
     }
   }
 });
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer;
